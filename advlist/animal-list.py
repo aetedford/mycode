404d0b1a89db056100be5de5a7fae0a0fdc1d7e7
Eric Tedford <aetedford@users.noreply.github.com>
2022-03-08 01:01:41 +0000

    manipulating lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" Lists of Lists
+    Lab 14 Step 33 - Optional Challenge """
+
+
+def main():
+    animals = ["fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+    print(animals)
+
+main()
