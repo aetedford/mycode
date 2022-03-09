aa3f195fe2d83e5eab1a12b31d6ed02a6ec0bc8e
Eric Tedford <aetedford@users.noreply.github.com>
2022-03-09 20:52:59 +0000

    Read in Data

--- cfg03.py
+++ cfg03.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 ## create file object in "r"ead mode
+
+count = 0
+
 with open("vlanconfig.cfg", "r") as configfile:
+    count += 1
     ## readlines() creates a list by reading target
     ## file line by line
     configlist = configfile.readlines()
@@ -9,4 +13,5 @@ with open("vlanconfig.cfg", "r") as configfile:
 
 ## each item of the list now has the "\n" characters back
 print(configlist)
+print(count)
 
