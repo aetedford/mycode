18c9821a0015f9f15c2f5a61886e0a1b47765109
Eric Tedford <aetedford@users.noreply.github.com>
2022-03-10 15:36:19 +0000

    Playing with python pip packages

---
+++ color/color2MINE.pyi
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Alta3 Research Class
+Learning how to use functions"""
+
+import crayons
+
+def main():
+    print(f"{crayons.red('Er')}{crayons.green('ic ')}{crayons.yellow('Te')}{crayons.blue('df')}{crayons.magenta('or')}{crayons.cyan('d')}")
+
+main()
